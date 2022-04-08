4040198e090c50af5fb05110e20d349f1ecb863f
rodukov <electroneuphoria@gmail.com>
2022-04-08 22:08:44 +0300

    CSS

--- src/App.vue
+++ src/App.vue
@@ -7,4 +7,5 @@ body { margin: 0; padding: 0; }
 
 /* Fonts */
 @import url('https://fonts.googleapis.com/css2?family=Karla:wght@700&display=swap');
+::selection { background: rgba(12, 187, 128, 0.5); }
 </style>
